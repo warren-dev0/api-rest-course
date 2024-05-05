be32d98706671b27910ccde5b0fdb8b3c3067737
Warren Acosta <infinitycode001@gmail.com>
2024-05-04 21:10:09 -0400

    finish class 4

--- models/local-file-system/movie.js
+++ models/local-file-system/movie.js
@@ -1,21 +1,21 @@
 import { randomUUID } from 'node:crypto';
-import { readJSON } from '../utils.js';
+import { readJSON } from '../../utils.js';
 const movies = readJSON('./movies.json');
 
 export class MovieModel {
-    static async getAll ({ genre }) {
+    static async getAll({ genre }) {
         if (genre) {
             return moviesRouter.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()));
         }
         return movies;
     };
 
-    static async getById ({ id }) {
+    static async getById({ id }) {
         const movie = movies.find(movie => movie.id === id)
         return movie;
     };
 
-    static async create ({input}) {
+    static async create({ input }) {
         const newMovie = {
             id: randomUUID(),
             ...input
@@ -29,7 +29,7 @@ export class MovieModel {
 
     };
 
-    static async update ({id, input}) {
+    static async update({ id, input }) {
         const movieIndex = movies.findIndex(movie => movie.id === id);
 
         if (movieIndex === -1) return false;
@@ -44,7 +44,7 @@ export class MovieModel {
         return movies[movieIndex];
     };
 
-    static async delete ({ id }) {
+    static async delete({ id }) {
         const movieIndex = movies.findIndex(movie => movie.id === id);
 
         if (movieIndex === -1) return false;
